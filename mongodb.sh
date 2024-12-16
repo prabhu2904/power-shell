97b43b48f1e0141fdf11bcd30449f43a5e296248
prabhu2904 <uyyalaprabhukumar77@gmail.com>
2024-12-16 14:34:02 -0800

    mangodb repo

--- mongodb.sh
+++ mongodb.sh
@@ -33,7 +33,7 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 
 VALIDATE $? "Copied MongoDB Repo"
 
-dnf install mongodb-org -y &>> $LOGFILE
+dnf install -y mongodb-org  &>> $LOGFILE
 
 VALIDATE $? "Installing MongoDB"
 
