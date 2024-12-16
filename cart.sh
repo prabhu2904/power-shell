c2b937548dfaef05fa4eeea491b9a78aa3b2189d
prabhu2904 <uyyalaprabhukumar77@gmail.com>
2024-12-16 15:15:41 -0800

    mangodb repo

--- cart.sh
+++ cart.sh
@@ -70,7 +70,7 @@ npm install  &>> $LOGFILE
 VALIDATE $? "Installing dependencies"
 
 # use absolute, because cart.service exists there
-cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service &>> $LOGFILE
+cp /home/14372/power-shell/cart.service /etc/systemd/system/cart.service &>> $LOGFILE
 
 VALIDATE $? "Copying cart service file"
 
